2e184ed5fe9ca96ae483fba94179105a42ed0ef9
besoeasy <besoeasy@users.noreply.github.com>
2024-05-07 10:20:31 +0530

    chore: Refactor HTML and JavaScript code formatting

--- index.js
+++ index.js
@@ -3,39 +3,43 @@ async function fetchData(file) {
 
     if (response.ok) {
         const textData = await response.text();
-        return textData.split('\n').filter(Boolean);
+        return textData.split("\n").filter(Boolean);
     } else {
-        console.error('Failed to load data from the text file.');
+        console.error("Failed to load data from the text file.");
         return [];
     }
 }
 
 (async function () {
-
-    const dataArray = await fetchData('pages.txt');
+    const dataArray = await fetchData("pages.txt");
 
     setTimeout(() => {
         const randomIndex = Math.floor(Math.random() * dataArray.length);
         const randomURL = dataArray[randomIndex];
-        window.location.replace(randomURL);
-    }, 1000 * 10);
 
+        const searchQuery = document.querySelector("#default-search").value;
+
+        if (!searchQuery) {
+            window.location.replace(randomURL);
+        }
+    }, 1000 * 10);
 })();
 
 (async function () {
-    document.addEventListener('DOMContentLoaded', async function () {
-        const searchEngines = await fetchData('search.txt');
-        const searchForm = document.querySelector('form');
+    document.addEventListener("DOMContentLoaded", async function () {
+        const searchEngines = await fetchData("search.txt");
+        const searchForm = document.querySelector("form");
 
-        searchForm.addEventListener('submit', function (event) {
+        searchForm.addEventListener("submit", function (event) {
             event.preventDefault();
 
-            const searchQuery = document.querySelector('#default-search').value;
+            const searchQuery = document.querySelector("#default-search").value;
 
             const randomIndex = Math.floor(Math.random() * searchEngines.length);
             const randomSearchEngine = searchEngines[randomIndex];
 
-            window.location.href = randomSearchEngine + encodeURIComponent(searchQuery);
+            window.location.href =
+                randomSearchEngine + encodeURIComponent(searchQuery);
         });
     });
 })();
